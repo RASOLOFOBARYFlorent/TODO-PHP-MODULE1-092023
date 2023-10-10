1f231d30bc4e68e48792d0c85935f44faa537ba1
RASOLOFOBARYFlorent <rasolofobaryflorent@gmail.com>
2023-10-10 18:27:53 +0200

    Premier version du projet php

---
+++ html/footer.php
@@ -0,0 +1,28 @@
+
+    <div class="footer">
+        
+        <h6>
+            My simple project to do list
+        </h6>
+        <div class="socialnetworkandtext">
+            <div class="socials">
+                <img src="image/icon_facebook_footer.png" alt="">
+                <img src="image/footer4.png" alt="">
+                <img src="image/footer6.png" alt="">
+                <img src="image/icon_twitter_footer.png" alt="">
+            </div>
+            <div class="text">
+                <h1>Quick links</h1>
+                <div class="links">
+                   <a href="#">Home</a>
+                   <a href="#">Facebook</a>
+                   <a href="#">Contact us</a>
+                   <a href="#">Email us</a>
+                </div>
+                
+            </div>
+            <img src="image/footer1.png" alt="" class="img12">
+        </div>
+    </div>
+</body>
+</html>
